29b895d86a750a04ef9a4e8ae5bda67ed1041c29
mengkongly <mengkong2009@yahoo.com>
2018-10-23 23:49:25 -0500

    Created model -a

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/products','ProductController');
+
+// uri: /products/11/reviews 
+Route::group(['prefix'=>'products'],function(){
+    Route::apiResource('/{product}/reviews','ReviewController');
+});
